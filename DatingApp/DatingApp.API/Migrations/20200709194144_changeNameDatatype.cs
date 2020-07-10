86d3739dd106ff0d50b5da1fef87a83fbd515ea9
Ali <Ali@LAPTOP-FUIB0GOV>
2020-07-09 22:33:28 -0500

    set up dotnet core middleware and routing also created angular Single Page Application (SPA) but not wired yet.

---
+++ DatingApp/DatingApp.API/Migrations/20200709194144_changeNameDatatype.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class changeNameDatatype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Values",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Values",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
