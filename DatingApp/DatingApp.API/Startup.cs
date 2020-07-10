29660ffaee3c1af25d84a56842d4c5ae1c82ed8c
Ali <Ali@LAPTOP-FUIB0GOV>
2020-07-09 23:35:10 -0500

    configure cors policy from Api project and also configured angular to query appi to retrieve data. not displaying yet but I can see from network tab data is coming through.

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -23,7 +23,10 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>
+            (x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
+
             services.AddControllers();
         }
 
@@ -42,8 +45,9 @@ namespace DatingApp.API
            
 
             // app.UseHttpsRedirection();
-
+             
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
