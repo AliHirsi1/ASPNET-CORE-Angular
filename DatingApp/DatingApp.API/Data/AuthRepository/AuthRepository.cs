9f124966e7706a87ab0aad9c5406c9ff89feb4ff
Ali <Ali@LAPTOP-FUIB0GOV>
2020-07-10 23:10:59 -0500

    created password hash and password salt. I also implemented Registration logic only.

--- DatingApp/DatingApp.API/Data/AuthRepository/AuthRepository.cs
+++ DatingApp/DatingApp.API/Data/AuthRepository/AuthRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 
@@ -5,6 +6,12 @@ namespace DatingApp.API.Data.AuthRepository
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
         public Task<bool> IsUserExists(string username)
         {
             throw new System.NotImplementedException();
@@ -15,9 +22,26 @@ namespace DatingApp.API.Data.AuthRepository
             throw new System.NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            byte [] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using( var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+           
         }
     }
 }
