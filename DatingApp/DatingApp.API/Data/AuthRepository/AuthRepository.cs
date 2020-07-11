10010174f2eb2b6c880462028425447f07825afa
Ali <Ali@LAPTOP-FUIB0GOV>
2020-07-10 23:29:13 -0500

    implemented password hasshing and salting as well checking if user exists beforing letting them to register to avoid duplicate users and login method as well.

--- DatingApp/DatingApp.API/Data/AuthRepository/AuthRepository.cs
+++ DatingApp/DatingApp.API/Data/AuthRepository/AuthRepository.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data.AuthRepository
 {
@@ -12,14 +14,43 @@ namespace DatingApp.API.Data.AuthRepository
             _context = context;
 
         }
-        public Task<bool> IsUserExists(string username)
+        public async Task<bool> IsUserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if(await _context.Users.AnyAsync(x => x.UserName == username)){
+                return true;
+            }
+            
+            return false;           
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+           var user = await _context.Users.Where(x => x.UserName == username).FirstOrDefaultAsync();
+
+           if(user == null){
+               return null;
+           }
+           
+           if(!verifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)){
+               return null;
+           }
+
+           return user;
+        }
+
+        private bool verifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+             using( var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {                
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i]){
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
